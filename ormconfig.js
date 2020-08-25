e60f2f80f251378a34f18cde16ca7b99945c9467
codewithmo <mohammedalimomin796@gmail.com>
2020-08-25 14:48:31 +0530

    ormconfig-->autoloadEntities true

--- ormconfig.js
+++ ormconfig.js
@@ -1,18 +1,17 @@
 module.exports = [
   {
-    "name": "default",
-    "type": "postgres",
-    "host": "localhost",
-    "port": 5435,
-    "username": "postgres",
-    "password": "mysecretpassword",
-    "database": "myrefill",
-    "entities": ["dist/**/*.entity.js"],
-    "migrations": [
-        "dist/migrations/**/*.js"
-      ],
-      "cli": {
-        "migrationsDir": "src/migrations"
-      }
-  }
-]
+    name: 'default',
+    type: 'postgres',
+    host: 'localhost',
+    port: 5435,
+    username: 'postgres',
+    password: 'mysecretpassword',
+    database: 'myrefill',
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/migrations/**/*.js'],
+    cli: {
+      migrationsDir: 'src/migrations',
+    },
+    autoLoadEntities: true,
+  },
+];
