3f42a422bc3f57578119e8f2a6e3e34469b89e30
Rajkumar S <rajkumaar2304@icloud.com>
2020-09-12 17:44:53 +0530

    Add Complaint entity (#7)
    * Add complaint entity
    
    * Add complaint migration
    
    * Remove redundant option in entity

---
+++ src/entities/complaint.entity.ts
@@ -0,0 +1,50 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Station } from './station.entity';
+import { User } from './user.entity';
+
+/**
+ * Represents Complaint
+ */
+@Entity()
+export class Complaint {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'uuid'})
+  stationId: string;
+
+  @ManyToOne(() => Station)
+  @JoinColumn({ name: 'stationId' })
+  station: Station;
+
+  @Column({ type: 'uuid'})
+  userId: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column()
+  comment: string;
+
+  @Column()
+  lastUpdatedBy: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deletedAt: Date;
+}
