9ac201d867a7d45139dce43dee5c313a0f9dbd33
Nikhil Mahendran <m_nikhil@outlook.com>
2020-10-25 23:45:10 +0530

    fix complaint list option

--- src/modules/forms/complaint.service.ts
+++ src/modules/forms/complaint.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CRUDService } from 'src/common/class/crud';
 import { Complaint } from '../../entities/complaint.entity';
+import { ComplaintListOption } from './dto/request/complaintListOption.dto';
 
 /**
  * ComplaintService has the CRUD operation for the complaint entity.
  */
 @Injectable()
-export class ComplaintService extends CRUDService<Complaint> {
+export class ComplaintService extends CRUDService<
+  Complaint,
+  ComplaintListOption
+> {
   Entity = Complaint;
 }
